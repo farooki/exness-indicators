8c08d677e4369326229589a5b2c6fe63c0dbfb41
Farooqi <muneebahmadfarooqi@gmail.com>
2026-08-18 03:15:34 +0500

    Add Asia session background indicator

---
+++ asia_session_background.py
@@ -0,0 +1,37 @@
+# indie:lang_version = 5
+
+from datetime import datetime
+from indie import indicator, plot, color
+
+
+@indicator('Asia Background', overlay_main_pane=True)
+
+@plot.background(
+    'Asia Background',
+    color=color.BLUE(alpha=0.08)
+)
+
+
+def Main(self):
+
+    current_time = datetime.utcfromtimestamp(self.time[0])
+
+    current_minutes = (
+        current_time.hour * 60
+        + current_time.minute
+    )
+
+    session_start = 0
+    session_end = 9 * 60
+
+    in_session = (
+        current_minutes >= session_start
+        and current_minutes < session_end
+    )
+
+    background_color = color.TRANSPARENT
+
+    if in_session:
+        background_color = color.BLUE(alpha=0.08)
+
+    return plot.Background(background_color)
